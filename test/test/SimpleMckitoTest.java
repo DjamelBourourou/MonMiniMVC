8fe01174612309ffd2574551c5f8a27b4840e4dc
Djamel <djamelbourourou@gmail.com>
2025-06-24 01:14:31 +0200

    Changement du nom dans le test

--- test/test/SimpleMckitoTest.java
+++ test/test/SimpleMckitoTest.java
@@ -1,6 +1,6 @@
 
 package test;
-
+import  javax.servlet.http.HttpServletResponse;
 import  javax.servlet.http.HttpServletRequest;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
@@ -9,9 +9,9 @@ public class SimpleMckitoTest {
     @Test
     public void testMockitoWorking(){
         HttpServletRequest req=mock(HttpServletRequest.class);
-        when(req.getParameter("nom")).thenReturn("Djamel");
+        when(req.getParameter("nom")).thenReturn("mehdi");
         assert
-                "Djamel".equals(req.getParameter("nom"));
+                "mehdi".equals(req.getParameter("nom"));
     }
     
 }
